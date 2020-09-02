49f6664ba2d4c6313d8a8b68bf28488461ed01ed
jtierno <john.tierno@outlook.com>
2020-09-02 07:30:19 -0400

    ADD Sql repo

---
+++ MyShop/MyShop.DataAccess.SQL/SQLRepository.cs
@@ -0,0 +1,65 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class SQLRepository<T> : IRepository<T> where T : BaseEntity
+    {
+
+        internal DataContext dataContext;
+        internal DbSet<T> dbSet;
+
+
+        public SQLRepository(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+            this.dbSet = dataContext.Set<T>();
+        }
+
+
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
+
+        public void Commit()
+        {
+            dataContext.SaveChanges();
+        }
+
+        public void Delete(string Id)
+        {
+            var t = Find(Id);
+
+            if (dataContext.Entry(t).State == EntityState.Detached)
+                dbSet.Attach(t);
+            
+            dbSet.Remove(t);
+
+        }
+
+        public T Find(string Id)
+        {
+            return dbSet.Find(Id);
+        }
+
+        public void Insert(T t)
+        {
+            dbSet.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            dbSet.Attach(t);
+            dataContext.Entry(t).State = EntityState.Modified;
+
+
+        }
+    }
+}
